6a8f62050d8a2a1d28500d8e7b4f486bed9b9309
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2022-01-14 12:05:49 +0100

    Update difference score

--- Recognize.py
+++ Recognize.py
@@ -1,14 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-from Localization import get_plate
 
 AMBIGUOUS_RESULT = "AMBIGUOUS"
 EPSILON = 0.15
 # Load the reference characters
 character_array = ['B', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'X', 'Z', '0', '1', '2',
-                 '3', '4', '5', '6', '7', '8', '9']
+                   '3', '4', '5', '6', '7', '8', '9']
 
 reference_characters = {}
 path = "TrainingSet/Categorie I/"
@@ -26,16 +24,20 @@ def loadImage(filepath, filename, grayscale=True):
 
 
 def difference_score(test_image, reference_character):
-    reference_character = cv2.resize(reference_character, (len(test_image[0]),len(test_image)))
-    
+    reference_character = cv2.resize(reference_character, (len(test_image[0]), len(test_image)))
     # return the number of non-zero pixels
     return np.count_nonzero(cv2.bitwise_xor(test_image, reference_character))
 
 
 def give_label_two_scores(test_image):
     # Get the difference score with each of the reference characters
+
+    # Erode to remove noise
+    test_image = cv2.erode(test_image, np.ones((2,2)))
+
     difference_scores = []
-    for key,value in reference_characters.items():
+    for key, value in reference_characters.items():
+        print("key",key,"score:",difference_score(test_image, value))
         difference_scores.append(difference_score(test_image, value))
 
     difference_scores = np.array(difference_scores)
@@ -43,13 +45,17 @@ def give_label_two_scores(test_image):
     result_char = 0
 
     # Check if the ratio of the two scores is close to 1 (if so return AMBIGUOUS_RESULT)
-    for key,value in reference_characters.items():
+    for key, value in reference_characters.items():
         if difference_score(test_image, value) == A:
+            plotImage(test_image)
+            plotImage(value)
             result_char = key
 
     ratio = A / B
-    if ratio > 1 - EPSILON and ratio < 1 + EPSILON:
-        return AMBIGUOUS_RESULT
+    print("ratio:", ratio)
+    # if ratio > 1 - EPSILON and ratio < 1 + EPSILON:
+    #     return AMBIGUOUS_RESULT
+
     # Return a single character based on the lowest score
     return result_char
 
@@ -72,8 +78,13 @@ Hints:
 
 
 def segment_and_recognize(plate_imgs):
+    # Call setup only once
     setup()
-    # print(reference_characters)
+    # Main functionality
+    main(plate_imgs)
+
+
+def main(plate_imgs):
     recognized_chars = []
     images = seperate(plate_imgs)
     print("ze zijn gesplit!!!!!!!!!!!!!")
@@ -81,13 +92,13 @@ def segment_and_recognize(plate_imgs):
         character = give_label_two_scores(image)
         if character != AMBIGUOUS_RESULT:
             recognized_chars.append(character)
-    print("recognized:",recognized_chars)
+    print("recognized:", recognized_chars)
     return recognized_chars
 
 
 def setup():
     # Setup reference characters
-    letter_counter = 1 # starts at 1.bmp
+    letter_counter = 1  # starts at 1.bmp
     number_counter = 0
     for char in character_array:
         if char.isdigit():
@@ -98,11 +109,9 @@ def setup():
             letter_counter = letter_counter + 1
 
     # Resize reference characters
+    for char,value in reference_characters.items():
+        reference_characters[char] = crop_to_boundingbox(value)
 
-
-    # test_images = []
-    # test_images.append(frame)
-    # segment_and_recognize(test_images)
 
 def crop_to_boundingbox(image):
     # plotImage(image)
@@ -121,9 +130,9 @@ def crop_to_boundingbox(image):
                     minj = j
                 if j > maxj:
                     maxj = j
-    return image[mini:maxi+1,minj:maxj+1]
+    return image[mini:maxi + 1, minj:maxj + 1]
 
-  
+
 def seperate(image):
     plotImage(image, "")
     # convert to grayscale
@@ -139,46 +148,46 @@ def seperate(image):
                 plate[i][j] = 0
 
     # use epsilon, this is
-    epsilon = 0.05*len(plate[0]) 
+    epsilon = 0.05 * len(plate[0])
 
     # after observing multiple plates, I saw that each character has a width of approximately 10% of the plate's width
-    charwidth = 0.1*len(plate[0])
-
+    charwidth = 0.1 * len(plate[0])
 
     boxes = []
     overlap = np.array([])
-    while(len(boxes)<6 and len(overlap)<len(plate[0])-int(charwidth)):
-        minwhite = 2*len(plate)
-        box = (0,0)
-        for j in range(int(len(plate[0])-charwidth-epsilon)):
+    while (len(boxes) < 6 and len(overlap) < len(plate[0]) - int(charwidth)):
+        minwhite = 2 * len(plate)
+        box = (0, 0)
+        for j in range(int(len(plate[0]) - charwidth - epsilon)):
             if j not in overlap:
-                whites = cv2.countNonZero(plate[:,j])
-                for jj in range(int(j+charwidth),int(j+charwidth+epsilon)):
-                    if (not np.in1d(overlap,np.arange(j,jj)).any()) and whites+cv2.countNonZero(plate[:,jj]) < minwhite:
-                        minwhite = whites+cv2.countNonZero(plate[:,jj])
-                        box = (j,jj)
-        overlap = np.concatenate((overlap, np.arange(box[0],box[1]+1)))
+                whites = cv2.countNonZero(plate[:, j])
+                for jj in range(int(j + charwidth), int(j + charwidth + epsilon)):
+                    if (not np.in1d(overlap, np.arange(j, jj)).any()) and whites + cv2.countNonZero(
+                            plate[:, jj]) < minwhite:
+                        minwhite = whites + cv2.countNonZero(plate[:, jj])
+                        box = (j, jj)
+        overlap = np.concatenate((overlap, np.arange(box[0], box[1] + 1)))
         boxes.append(box)
 
     boxes.sort()
     gaps = []
-    for i in range(len(boxes)-1):
-        gaps.append(boxes[i+1][0]-boxes[i][1])
+    for i in range(len(boxes) - 1):
+        gaps.append(boxes[i + 1][0] - boxes[i][1])
     gaps.sort()
     gap1 = gaps[-1]
     gap2 = gaps[-2]
 
-    dot1 = (0,0)
-    dot2 = (0,0)
-    for i in range(len(boxes)-1):
-        gap = boxes[i+1][0]-boxes[i][1]
-        if dot1 == (0,0) and gap1 == gap:
-            dot1 = (boxes[i+1][0],boxes[i][1])
-        elif dot2 == (0,0) and gap2 == gap:
-            dot2 = (boxes[i+1][0],boxes[i][1])
+    dot1 = (0, 0)
+    dot2 = (0, 0)
+    for i in range(len(boxes) - 1):
+        gap = boxes[i + 1][0] - boxes[i][1]
+        if dot1 == (0, 0) and gap1 == gap:
+            dot1 = (boxes[i + 1][0], boxes[i][1])
+        elif dot2 == (0, 0) and gap2 == gap:
+            dot2 = (boxes[i + 1][0], boxes[i][1])
 
-    dot1index = int((dot1[0]+dot1[1])/2)
-    dot2index = int((dot2[0]+dot2[1])/2)
+    dot1index = int((dot1[0] + dot1[1]) / 2)
+    dot2index = int((dot2[0] + dot2[1]) / 2)
 
     black = True
     matches = []
@@ -192,35 +201,32 @@ def seperate(image):
                 matches[-1].append(i)
                 black = True
 
-
-    finalindex = int(len(plate)/2)
+    finalindex = int(len(plate) / 2)
     if len(matches) > 0:
         if len(matches[-1]) == 1:
             matches[-1].append(len(plate))
-        center = float(len(plate)/2)
+        center = float(len(plate) / 2)
         mindis = len(plate)
         for m in matches:
-            if np.abs(center-(float((m[1]+m[0])/2.0))) < mindis:
-                mindis = np.abs(center-(float((m[1]+m[0])/2.0)))
-                finalindex = int((m[1]+m[0])/2)
+            if np.abs(center - (float((m[1] + m[0]) / 2.0))) < mindis:
+                mindis = np.abs(center - (float((m[1] + m[0]) / 2.0)))
+                finalindex = int((m[1] + m[0]) / 2)
 
-    heightchar = float(0.17*len(plate[0]))
-    if finalindex < float(heightchar/2.0) or finalindex > len(plate)-float(heightchar/2.0):
-        finalindex = int(len(plate)/2)
+    heightchar = float(0.17 * len(plate[0]))
+    if finalindex < float(heightchar / 2.0) or finalindex > len(plate) - float(heightchar / 2.0):
+        finalindex = int(len(plate) / 2)
 
-    finalindex += int(0.02*len(plate))
-    ymin = finalindex-int(heightchar/2)
-    ymax = finalindex+int(heightchar/2)
+    finalindex += int(0.02 * len(plate))
+    ymin = finalindex - int(heightchar / 2)
+    ymax = finalindex + int(heightchar / 2)
 
     characters = []
     for box in boxes:
-        char = plate[ymin:ymax,box[0]:box[1]]
+        char = plate[ymin:ymax, box[0]:box[1]]
         # eventueel dilate en erode om mooier te maken
         char = crop_to_boundingbox(char)
         characters.append(char)
     return characters
-
-
 
     # for i in range(len(image)):
     #     for j in range(len(image[0])):
@@ -229,12 +235,9 @@ def seperate(image):
     #         else:
     #             image[i][j] = [0,0,0]
 
-
-
     # image[:,dot1index] = [0,255,0]
     # image[:,dot2index] = [0,255,0]
     # plotImage(image, "")
-
 
     # mark found positions green
     # for box in boxes:
@@ -280,8 +283,6 @@ def seperate(image):
     #     image[:, split] = [0, 255, 0]
     # plotImage(image, "")
 
-
-
     # contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     # for c in contours:    
     #     pixels = np.zeros((len(image),len(image[0])))
@@ -292,30 +293,28 @@ def seperate(image):
     # ret2,th2 = cv2.threshold(plate,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # plotImage(th2, "thresh")
 
-
-
-    # 
+    #
     # biggest = contours[0]
     # for c in contours:
     #     if len(c) > len(biggest):
     #         biggest = c
-        # imin = len(plate)
-        # imax = 0
-        # jmin = len(plate[0])
-        # jmax = 0
-        # pixels = np.zeros((len(plate),len(plate[0])))
-        # for p in c:
-        #     pixels[p[0][1]][p[0][0]] = 255
-        #     if p[0][1] < imin:
-        #         imin = p[0][1]
-        #     if p[0][1] > imax:
-        #         imax = p[0][1]
-        #     if p[0][0] < jmin:
-        #         jmin = p[0][0]
-        #     if p[0][0] > jmax:
-        #         jmax = p[0][0]
-        #     if imax - imin < 0.25*len(plate) or jmax - jmin < 0.025*len(plate[0]):
-        #         plate[imin:imax,jmin:jmax] = 0
+    # imin = len(plate)
+    # imax = 0
+    # jmin = len(plate[0])
+    # jmax = 0
+    # pixels = np.zeros((len(plate),len(plate[0])))
+    # for p in c:
+    #     pixels[p[0][1]][p[0][0]] = 255
+    #     if p[0][1] < imin:
+    #         imin = p[0][1]
+    #     if p[0][1] > imax:
+    #         imax = p[0][1]
+    #     if p[0][0] < jmin:
+    #         jmin = p[0][0]
+    #     if p[0][0] > jmax:
+    #         jmax = p[0][0]
+    #     if imax - imin < 0.25*len(plate) or jmax - jmin < 0.025*len(plate[0]):
+    #         plate[imin:imax,jmin:jmax] = 0
 
 # setup()
 # for image in plate_imgs:
